ec3824d10de35da222e59a22c513026f9fd01200
Satya37x1112 <manoharisatyasarthak@gmail.com>
2026-05-08 12:52:33 +0530

    Update: satya.cpp

---
+++ Far-Manager-CP/satya.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Your logic here
+	cout<<"Hello Everyone I am Satya Sarthak Manohari currently pursuing my bachelors in CSE and my specialization is cyber security and I love competative programming because it's too challenging and I am the only one in my college to code in c++ and I do believe that I will crack  GATE exam and land into IIT DELHI and in 2029 can participate in ICPC See u there . "<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t = 1;
+    // cin >> t; // Uncomment if there are multiple test cases
+    while(t--) {
+        solve();
+    }
+    cout << "\nPress Enter to exit...";
+    cin.ignore();
+    cin.get();
+    return 0;
+}
